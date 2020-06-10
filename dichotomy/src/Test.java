0ce6effa3d0159fee48224e3dac3d4a90cea5674
dengliqiang <dada@163.com>
2020-06-10 17:21:07 +0800

    1.添加递归实现 2.第二种算法 3.备注说明

--- dichotomy/src/Test.java
+++ dichotomy/src/Test.java
@@ -3,29 +3,34 @@ package src;
 import java.util.Arrays;
 
 /**
- * @Description  二分法查找
+ * @Description 二分法查找
  * @Date 2020/6/8 15:49
  * @Created by dlq
  */
 public class Test {
     public static void main(String[] args) {
         //在使用二分法查找的时候，首先要将数据进行排序操作，
-        int[] arr={7,5,10,32,11,67,2,1,66,4,6,9,77,55,101,89};
+        int[] arr = {7, 5, 10, 32, 11, 67, 2, 1, 66, 4, 6, 9, 77, 55, 101, 89};
         //排序
         Arrays.sort(arr);
-        Arrays.stream(arr).forEach(item ->{
-            System.out.print(item+" ");
+        Arrays.stream(arr).forEach(item -> {
+            System.out.print(item + " ");
         });
         //通过二分法查询排序后的，某个数的位置,若不存在，返回-1
-        int index =  dichotomyLoop(arr,66);
-        System.out.println("index=== "+index);
+        int index = dichotomyLoop(arr, 66);
+        System.out.println("index=== " + index);
+
+        System.out.println("======================= ");
+
+        int index2 = dichotomyRecursion(0, arr.length - 1, arr, 68);
+        System.out.println("index=== " + index2);
     }
 
     /**
-     * @description 循环方式  通过二分法查询排序后的，某个数的位置,若不存在，返回-1
      * @param arr
-     * @param: num
      * @return int
+     * @description 循环方式  通过二分法查询排序后的，某个数的位置,若不存在，返回-1
+     * @param: num
      * @author dlq
      * @date 2020/6/8 17:26
      */
@@ -33,16 +38,19 @@ public class Test {
 
         //个人理解：在二分法中，开始索引位置和结束索引位置一直在变动，中间索引随着开始和结束在变动，
         //所以，初始化开始索引和结束索引，然后在比较中，变动开始索引和结束索引
-        int high=arr.length - 1 ;
+        int high = arr.length - 1;
         int low = 0;
-        while (low <= high){
+        while (low <= high) {
             //二分法，从中间开始比较
-            int mid = (low+high)/2;
-            if(arr[mid] > num ){//中间之前
-               high = mid -1;
-            }else if (arr[mid] < num){//中间之后
-                low = mid +1;
-            }else {
+            //第一种：
+           // int mid = (low + high) / 2;
+           //第二种
+            int mid = low + (high - low)/2;
+            if (arr[mid] > num) {//num在中间之前
+                high = mid - 1;
+            } else if (arr[mid] < num) {//num在中间之后
+                low = mid + 1;
+            } else {
                 return mid;
             }
         }
@@ -50,13 +58,30 @@ public class Test {
     }
 
     /**
-     * @description 递归  通过二分法查询排序后的，某个数的位置,若不存在，返回-1
-     * @param
+     * @param low 可能变动的开始索引位置
      * @return int
-     * @author dengliqiang
-     * @date 2020/6/9 19:43
+     * @description 递归  通过二分法查询排序后的，某个数的位置,若不存在，返回-1
+     * @param: high 可能变动的结束索引位置
+     * @param: arr  传入的数组
+     * @param: num  我们要查找的对象
+     * @author dlq
+     * @date 2020/6/9 19:47
      */
-    private static  int dichotomyRecursion(){
-        return  0;
+    private static int dichotomyRecursion(int low, int high, int[] arr, int num) {
+        if (low > high) return -1;
+        int mid = (low + high) / 2;
+        if (arr[mid] > num) { //num在中间之前
+            //单独取出来，更加明了,high的变化
+            high = mid - 1;
+            return dichotomyRecursion(low, high, arr, num);
+        }
+        if (arr[mid] < num) {//num在中间之后
+            //单独取出来，更加明了，low的变化
+            low = mid + 1;
+            return dichotomyRecursion(low, high, arr, num);
+        }
+        if (arr[mid] == num)
+            return mid;
+        return -1;
     }
 }
